6b407a3c468db7418802f0b7ded2b890b54d4f63
Uwe Prehn <keyara@gmx.de>
2024-06-20 21:13:35 +0200

    created HiddenTripletConstraint class

---
+++ SimpleSudoku.ConstraintLibrary/Constraints/HiddenTripletConstraint.cs
@@ -0,0 +1,14 @@
+﻿using SimpleSudoku.CommonLibrary.Models;
+
+namespace SimpleSudoku.ConstraintLibrary.Constraints
+{
+    public class HiddenTripletConstraint(IPuzzleModel puzzle) : Constraint
+    {
+        private readonly IPuzzleModel _puzzle = puzzle;
+
+        public override bool ApplyConstraint(out string errorMessage)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
